1f71e179d300c2db29460ce8283434dfbaaadc61
root <root@guest.guest>
2024-04-22 15:25:51 +0000

    auto-update from dotfiles

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,10 @@
+return {
+    "numToStr/Comment.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+        -- add any options here
+    },
+    lazy = false,
+}
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,10 @@
+return {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+        "mfussenegger/nvim-dap",
+        "nvim-neotest/nvim-nio",
+        "nvim-treesitter/nvim-treesitter",
+        "theHamsta/nvim-dap-virtual-text",
+        "jbyuki/one-small-step-for-vimkind",
+    },
+}
