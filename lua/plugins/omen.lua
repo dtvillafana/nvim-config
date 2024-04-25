f2e43aaed1dff0afa543b01a1d41917376df04df
root <root@guest.guest>
2024-04-25 20:07:00 +0000

    auto-update from dotfiles repo

--- lua/plugins/omen.lua
+++ lua/plugins/omen.lua
@@ -15,25 +15,25 @@ local path = function()
     end
 end
 return {
-    "https://github.com/nacro90/omen.nvim",
+    "dtvillafana/omen.nvim",
     dependencies = {
         "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim",
     },
     opts = {
-        picker = "telescope", --- Picker type
-        title = "Omen", --- Title to be displayed on the picker
+        picker = "telescope", -- Picker type
+        title = "Omen", -- Title to be displayed on the picker
         store = path(),
-        passphrase_prompt = "Passphrase: ", --- Prompt when asking the passphrase
-        register = "+", --- Which register to fill after decoding a password
-        retention = 45, --- How much seconds the life of the decoded passwords in the register
-        ignored = { --- Ignored directories or files that are not to be listed in picker
+        passphrase_prompt = "Passphrase: ", -- Prompt when asking the passphrase
+        register = "+", -- Which register to fill after decoding a password
+        retention = 45, -- Delay before password is cleared from the register
+        ignored = { -- Ignored directories or files that are not to be listed in picker
             ".git",
             ".gitattributes",
             ".gpg-id",
             ".stversions",
             "Recycle Bin",
         },
-        use_default_keymaps = true, --- Whether display info messages or not
+        use_default_keymaps = true, -- Whether display info messages or not
     },
 }
