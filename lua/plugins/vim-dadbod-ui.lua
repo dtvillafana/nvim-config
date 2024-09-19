fb964eb228b5929b60c93415b219d8da033c44e3
root <root@guest.guest>
2024-09-19 16:37:05 +0000

    auto-update from dotfiles repo

--- lua/plugins/vim-dadbod-ui.lua
+++ lua/plugins/vim-dadbod-ui.lua
@@ -1,28 +1,52 @@
 return {
-    "kristijanhusak/vim-dadbod-ui",
+    'kristijanhusak/vim-dadbod-ui',
     dependencies = {
-        { "tpope/vim-dadbod", lazy = true },
-        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "sqlserver", "plsql", "sqlite" }, lazy = true },
+        { 'tpope/vim-dadbod', lazy = true },
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'sqlserver', 'plsql', 'sqlite' }, lazy = true },
     },
     cmd = {
-        "DBUI",
-        "DBUIToggle",
-        "DBUIAddConnection",
-        "DBUIFindBuffer",
+        'DBUI',
+        'DBUIToggle',
+        'DBUIAddConnection',
+        'DBUIFindBuffer',
     },
     init = function()
         vim.g.db_ui_use_nerd_fonts = 1
         vim.g.db_ui_show_database_icon = 1
-        -- to make sql server work you might have to URL encode special characters and/or add some parameters to the end of your connection string:
-        -- vim.g.dbs = { dev2 = 'sqlserver://username:password@192.168.0.1:1433/MyDBName?Encrypt=1;trustServerCertificate=1' }
-        vim.g.dbs = { politics = "sqlite://" .. os.getenv("HOME") .. "/Sync/datasets/politics.db" }
+        local dbs = {}
+        local env_vars = {
+            {
+                env_var = os.getenv('sql3_pw'),
+                key = 'fics',
+                value_func = function(var)
+                    -- to make sql server work you might have to URL encode special characters and/or add some parameters to the end of your connection string:
+                    return 'sqlserver://sa:' .. var .. '@172.20.102.60:1433/fics_test?Encrypt=1;trustServerCertificate=1'
+                end,
+            },
+            {
+                env_var = os.getenv('HOME'),
+                key = 'politics',
+                value_func = function(var)
+                    return 'sqlite://' .. var .. '/Sync/datasets/politics.db'
+                end,
+            },
+        }
+
+        for _, item in ipairs(env_vars) do
+            if item.env_var ~= nil then
+                dbs = vim.tbl_deep_extend('force', dbs, {
+                    [item.key] = item.value_func(item.env_var)
+                })
+            end
+        end
+        vim.g.dbs = dbs
         vim.g.db_ui_use_nvim_notify = 0
 
         -- enable completion using cmp, I tried to make the autocmd fire on FileType but that just doesn't work
-        vim.api.nvim_create_autocmd("BufEnter", {
-            pattern = { "*.sql", "*.tsql", "*.plsql", "*-query-*" },
+        vim.api.nvim_create_autocmd('BufEnter', {
+            pattern = { '*.sql', '*.tsql', '*.plsql', '*-query-*' },
             callback = function()
-                require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+                require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
             end,
         })
     end,
